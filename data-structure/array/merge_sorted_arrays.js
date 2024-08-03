268db5c68e1f8312cb5fea572f913156957979cc
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-08-03 15:21:52 +0200

    merge sorted array

---
+++ data-structure/array/merge_sorted_arrays.js
@@ -0,0 +1,29 @@
+function mergeSortedArrays(arr1, arr2) {
+  let index1 = 0;
+  let index2 = 0;
+  let result = [];
+  while (index1 < arr1.length && index2 < arr2.length) {
+    if (arr1[index1] <= arr2[index2]) {
+      result.push(arr1[index1]);
+      index1++;
+    } else {
+      result.push(arr2[index2]);
+      index2++;
+    }
+  }
+
+  let leftInArr;
+  if (index1 < arr1.length) {
+    leftInArr = arr1.slice(index1);
+  }
+
+  if (index2 < arr2.length) {
+    leftInArr = arr2.slice(index2);
+  }
+
+  result = result.concat(leftInArr);
+  console.log(result);
+}
+
+mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]);
+// [0, 3, 4, 4, 6, 30, 31]
