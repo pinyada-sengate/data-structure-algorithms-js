a17ec1a262db6f863db67a3e912a2d3ec58febea
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-01-19 20:45:04 +0100

    linked list prepend

--- data-structure/linked-list/LinkedList.js
+++ data-structure/linked-list/LinkedList.js
@@ -17,9 +17,19 @@ class LinkedList {
     this.tail = node;
     this.length++;
   }
+
+  prepend(value) {
+    let node = {
+      value: value,
+      next: this.head,
+    };
+    this.head = node;
+    this.length++;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.prepend(1);
 console.log(myLinkedList);
